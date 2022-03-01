fd0ecb20814b7b943e91217e7a3c1107ca5eb603
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-03-01 15:43:29 -0500

    Add an ExperienceForm componenet for every work experience object
    Map over this.state.workExperience to give each object the necessary props

--- src/components/Work.js
+++ src/components/Work.js
@@ -7,6 +7,19 @@ class Work extends Component {
 
   render() {
     const { workExperience, changeWork, addWork, deleteWork } = this.props;
+    const workExperiences = workExperience.map((object, index) => {
+      return (
+        <ExperienceForm
+          key={object.id}
+          id={object.id}
+          workExperience={workExperience}
+          changeWork={changeWork}
+          addWork={addWork}
+          deleteWork={deleteWork}
+          isLast={index === workExperience.length - 1}
+        />
+      );
+    });
   }
 }
 
